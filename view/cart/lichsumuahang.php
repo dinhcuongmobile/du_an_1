e3522b632e2523cc4bd63362967cd5fd2903b30b
ThienGiap <nguyenthiengiap04@gmail.com>
2023-11-28 17:20:53 +0700

    Giap

--- view/cart/lichsumuahang.php
+++ view/cart/lichsumuahang.php
@@ -1,28 +1,56 @@
-<div class="cart-area pt-100 pb-100">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">     
-                <form action="#">
-                    <div class="cart-table-content">
-                        <div class="table-content table-responsive">
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th class="width-thumbnail">Mã hóa đơn</th>
-                                        <th class="width-name">Sản phẩm</th>
-                                        <th class="width-price">Đơn giá</th>
-                                        <th class="width-quantity">Số lượng</th>
-                                        <th class="width-subtotal">Trạng thái</th>
-                                        <th class="width-remove">Ngày đặt hàng</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                </tbody>
-                            </table>
+<section class="h-100 gradient-custom">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div style="width: 100%; font-size: 20px" class="col-lg-10 col-xl-8">
+                <div class="card" style="border-radius: 10px;">
+                    <div class="card-header px-4 py-5">
+                        <h5 class="text-muted mb-0">Cảm ơn <span style="color: red;">Cường</span> đã mua hàng</h5>
+                    </div>
+                    <div class="card-body p-4">
+                        <div class="d-flex justify-content-between align-items-center mb-4">
+                            <p class="lead fw-normal mb-0" style="color: red;">Đơn hàng đã mua</p>
+                            <select style="width: 12%; height: 30px; border: solid 1px #ced4da; border-radius: 5px;" name="" id="">
+                                <option value="">Tất cả</option>
+                                <option value="">Chờ xác nhận</option>
+                                <option value="">Đang giao</option>
+                                <option value="">Hoàn thành</option>
+                            </select>
+                        </div>
+                        <div class="card shadow-0 border mb-4">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <img src="../uploads/24.webp" class="img-fluid" alt="Phone">
+                                    </div>
+                                    <div style="width: 40.666667%;" class="col-md-2 text-center d-flex justify-content-center align-items-center">
+                                        <a style="font-size:20px" class="text-muted mb-0">Samsung Galaxy</a>
+                                    </div>
+                                    <div class="col-md-2 text-center d-flex justify-content-center align-items-center">
+                                        <p class="text-muted mb-0 small">Số lượng: 1</p>
+                                    </div>
+                                    <div style="width: 22%; display: flex;" class="col-md-2 text-center justify-content-center align-items-center">
+                                        <p style="font-size: 18px;" class="text-muted mb-0">Tông tiền: 17.000.000đ</p>
+                                    </div>
+                                    <hr class="mb-4" style="background-color: #e0e0e0; opacity: 1;">
+                                    <div style="display: inline-block;" class="row align-items-center">
+                                        <div class="col-md-2">
+                                            <p style="width: 500px;" class="text-muted mb-0 small">Trạng thái: Đã hoàn thành</p>
+                                        </div>
+                                    </div>
+                                    <div style="margin-top: 30px; width: 40%;" class="mb-1">
+                                        <h3>Mã hóa đơn: abcxyz</h3>
+                                    </div>
+                                    <div style="display: inline-block; padding: 0px; width: 56.5%;" class="cart-shiping-update-wrapper">
+                                        <div class="cart-shiping-update btn-hover">
+                                            <a style="float: right;" href="#">Mua lại</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</section>
